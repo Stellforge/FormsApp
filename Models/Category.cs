1e4c2051f9b75b24401f790d902eb9679da7911b
Furkan Erçelebi <stellforge6@gmail.com>
2025-07-15 17:21:29 +0300

    Proje dosyası ekle.

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace FormsApp.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; } //prop yazınca direk veriyor
+        public string Name { get; set; } = string.Empty;
+    }
+}
